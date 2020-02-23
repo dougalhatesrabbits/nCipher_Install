5251dc22d32bea3b1e8881bef9ac5390b0c49242
MightyBoosh <david.brooke14@icloud.com>
2020-02-23 11:51:47 +0000

    json

---
+++ src/test/java/LinuxTest.java
@@ -0,0 +1,28 @@
+import junit.framework.TestCase;
+
+public class LinuxTest extends TestCase {
+
+    public void testGetIsoChoices() {
+    }
+
+    public void testCheckMount() {
+    }
+
+    public void testCheckUsers() {
+    }
+
+    public void testGetTars() {
+    }
+
+    public void testUnpackSecWorld() {
+    }
+
+    public void testGetSecWorld() {
+    }
+
+    public void testTestGetSecWorld() {
+    }
+
+    public void testApplyDrivers() {
+    }
+}
---
+++ src/test/java/com/file/UnTarFileTest.java
@@ -0,0 +1,18 @@
+package com.file;
+
+import junit.framework.TestCase;
+
+public class UnTarFileTest extends TestCase {
+
+    public void testUnTarFile() {
+    }
+
+    public void testDeCompressGZipFile() {
+    }
+
+    public void testGetFileName() {
+    }
+
+    public void testUnzip() {
+    }
+}
